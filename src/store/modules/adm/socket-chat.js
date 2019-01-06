640c410656d8437a6c9236aa4cc6cd6095b0646d
RomanSlep <slep48@gmail.com>
2019-01-06 22:54:27 +0300

    proxy ssl wss

--- src/store/modules/adm/socket-chat.js
+++ src/store/modules/adm/socket-chat.js
@@ -11,14 +11,15 @@ export default (context) => {
 	const address = context.rootState.address
 	// const socket = io('ws://51.15.245.173:36668')
 	
-	
+	// const domain='sockets-test.adamant.im';
+	const domain='51.15.245.173';
 	let socket;
 	if (window.location.protocol != "https:") {
 		console.log('port ws', 36668)
-		socket = io('ws://51.15.245.173:36668');
+		socket = io('ws://'+domain+':36668');
 		} else {
 		console.log('port wss', 36669)
-		socket = io('wss://51.15.245.173:36669')
+		socket = io('wss://sockets-test.adamant.im')
 	}
 	
 	socket.on('connect', () => {
@@ -39,61 +40,60 @@ export default (context) => {
 					
 					Vue.set(context.rootState.newChats, targetChat[0].partner, 1)
 					if (context.rootState.notifySound) {
-	try {
-	window.audio.playSound('newMessageNotification')
-	} catch (e) {}
-	}
-	context.rootState.totalNewChats = context.rootState.totalNewChats + 1
-	} else {
-	
-	let newMessages = context.rootState.newChats[targetChat[0].partner]
-	if (!newMessages) {
-	newMessages = 0
-	}
-	if (context.rootState.notifySound) {
-	try {
-	window.audio.playSound('newMessageNotification')
-	} catch (e) {}
-	}
-	
-	Vue.set(context.rootState.newChats, targetChat[0].partner, newMessages + 1)
-	context.rootState.totalNewChats = context.rootState.totalNewChats + 1
-	const chats = context.rootGetters.getChats[targetChat[0].partner];
-	const txup = admApi.decodeChat(tx, tx.senderPublicKey);
-	txup.direction = "to";
-	chats.messages[txup.id] = txup;
-	
-	chats.last_message = {
-	confirm_class: "confirmed",
-	direction: "to",
-	id: txup.id,
-	message: txup.message,
-	timestamp: txup.timestamp
-	}
-	}
-	}
-	
-	} else {
-	console.log('Отчет о доставке')
-	targetChat = Object.values(chats).filter(chat => chat.partner === tx.recipientId)
-	if (targetChat.length > 0 && targetChat[0].messages[tx.id]) {
-	Vue.set(targetChat[0].messages[tx.id], 'confirm_class', 'confirmed')
-	}
-	}
-	// Update last chat message
-	Vue.set(targetChat, 'last_message', {
-	...targetChat.last_message,
-	message: i18n.t('chats.' + (tx.recipientId === context.rootGetters.getAdmAddress ? 'received_label' : 'sent_label')) + ' ' + tx.amount / 100000000 + ' ADM',
-	confirm_class: 'confirmed',
-	timestamp: utils.epochTime()
-	})
-	context.commit('transactions', [tx])
+						try {
+							window.audio.playSound('newMessageNotification')
+						} catch (e) {}
+					}
+					context.rootState.totalNewChats = context.rootState.totalNewChats + 1
+					} else {
+					
+					let newMessages = context.rootState.newChats[targetChat[0].partner]
+					if (!newMessages) {
+						newMessages = 0
+					}
+					if (context.rootState.notifySound) {
+						try {
+							window.audio.playSound('newMessageNotification')
+						} catch (e) {}
+					}
+					
+					Vue.set(context.rootState.newChats, targetChat[0].partner, newMessages + 1)
+					context.rootState.totalNewChats = context.rootState.totalNewChats + 1
+					const chats = context.rootGetters.getChats[targetChat[0].partner];
+					const txup = admApi.decodeChat(tx, tx.senderPublicKey);
+					txup.direction = "to";
+					chats.messages[txup.id] = txup;
+					
+					chats.last_message = {
+						confirm_class: "confirmed",
+						direction: "to",
+						id: txup.id,
+						message: txup.message,
+						timestamp: txup.timestamp
+					}
+				}
+			}
+			
+			} else {
+			console.log('Отчет о доставке')
+			targetChat = Object.values(chats).filter(chat => chat.partner === tx.recipientId)
+			if (targetChat.length > 0 && targetChat[0].messages[tx.id]) {
+				Vue.set(targetChat[0].messages[tx.id], 'confirm_class', 'confirmed')
+			}
+		}
+		// Update last chat message
+		Vue.set(targetChat, 'last_message', {
+			...targetChat.last_message,
+			message: i18n.t('chats.' + (tx.recipientId === context.rootGetters.getAdmAddress ? 'received_label' : 'sent_label')) + ' ' + tx.amount / 100000000 + ' ADM',
+			confirm_class: 'confirmed',
+			timestamp: utils.epochTime()
+		})
+		context.commit('transactions', [tx])
 	})
 	
 	socket.on('reconnecting', data => { // неудачный рекконект
-	console.log(address + ' recconecting...')
+		console.log(address + ' recconecting...')
 	})
 	
 	return socket
-	}
-		
+}
